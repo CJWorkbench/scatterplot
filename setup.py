30b4759e2909ad5b98c476d6b41345b2970e0e01
Adam Hooper <adam@adamhooper.com>
2019-08-07 14:42:50 -0400

    test with pandas 0.25.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-    name='scatterplot',
-    version='0.0.1',
-    description='Present data series in a scattered plot',
-    author='Erin Riglin',
-    author_email='er2865@columbia.edu',
-    url='https://github.com/CJWorkbench/scatterplot',
-    packages=[''],
-    py_modules=['scatterplot'],
-    install_requires=['pandas==0.24.1']
+    name="scatterplot",
+    version="0.0.1",
+    description="Present data series in a scattered plot",
+    author="Erin Riglin",
+    author_email="er2865@columbia.edu",
+    url="https://github.com/CJWorkbench/scatterplot",
+    packages=[""],
+    py_modules=["scatterplot"],
+    install_requires=["pandas==0.25.0"],
 )
