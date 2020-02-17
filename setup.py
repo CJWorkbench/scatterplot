6b7b76a8429b8a35eb396b33277794882f8bb82f
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-02-17 20:37:42 +0200

    Add min cjwmodule version to setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     url="https://github.com/CJWorkbench/scatterplot",
     packages=[""],
     py_modules=["scatterplot"],
-    install_requires=["pandas==0.25.0"],
+    install_requires=["pandas==0.25.0", "cjwmodule>=1.3.0"],
 )
